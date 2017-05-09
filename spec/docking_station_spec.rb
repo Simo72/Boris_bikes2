e96c3b8a894cedec7723120bcb6f28fd94cf4f92
Michael Jacobson <michael.jacobson89@gmail.com>
2017-05-09 14:58:31 +0100

    amended 'release_bike' and 'working' methods

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,3 +3,15 @@ require 'docking_station.rb'
 describe DockingStation do
   it { is_expected.to respond_to :release_bike }
 end
+
+describe DockingStation do
+  it "'releases' an instance of the Bike class" do
+    expect(subject.release_bike.class).to eq Bike
+  end
+end
+
+describe DockingStation do
+  it "'releases' a working bike" do
+    expect(subject.release_bike.working?).to eq true
+  end
+end
