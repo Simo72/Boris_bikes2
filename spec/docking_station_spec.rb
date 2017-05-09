ae424600588fa23bc5ec29848e1fdb26cd7b1224
Michael Jacobson <michael.jacobson89@gmail.com>
2017-05-09 16:30:16 +0100

    finished exercise 11

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -15,3 +15,26 @@ describe DockingStation do
     expect(subject.release_bike.working?).to eq true
   end
 end
+
+describe DockingStation do
+  it { is_expected.to respond_to(:dock).with(1).argument }
+end
+
+describe DockingStation do
+ it { is_expected.to respond_to :bike }
+end
+
+describe DockingStation do
+  it "docks something" do
+    bike = Bike.new
+    expect(subject.dock(bike)).to eq bike
+  end
+end
+
+describe DockingStation do
+  it "returns docked bikes" do
+    bike = Bike.new
+    subject.dock(bike)
+    expect(subject.bike).to eq bike
+  end
+end
