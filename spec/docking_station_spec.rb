24ee688d10b2cacab5cfa929faae53769d76d687
IAN SIMPSON <ianconradsimpson@gmail.com>
2017-05-10 16:49:29 +0100

    Step 11, final test complete

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -16,12 +16,14 @@ describe DockingStation do
     it "raises error when no bikes available" do
       expect{ subject.release_bike }.to raise_error 'No bikes available'
     end
+
+    it "'releases' a working bike" do
+      bike = Bike.new
+      subject.dock(bike)
+      expect(subject.release_bike.working?).to eq true
+    end
   end
 
-
-  it "'releases' a working bike" do
-    expect(subject.release_bike.working?).to eq true
-  end
 
   it "docks something" do
     bike = Bike.new
--- user_stories.md
+++ user_stories.md
@@ -8,7 +8,8 @@ Bike            | bike_is_working?
 Docking Station | release_a_bike
                 | dock_a_bike
                 | bike_available?
-                |
+                | dock_at_capacity?
+Maintainer      |
 
 
 ## Relationship Diagrams
@@ -22,3 +23,5 @@ Bike <-- bike_is_docked? --> true/false
 Docking Station <-- dock_a_bike --> Bike
 
 Docking Station <-- bike_available? --> true/false
+
+Docking Station <-- dock_at_capacity --> true/false
