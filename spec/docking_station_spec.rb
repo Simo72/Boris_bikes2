3dc966d74e7d57d022b7ae787cd91c9819a2cdb6
Mário Ribeiro <mail@marioribeiro.com>
2017-05-12 11:29:54 +0100

    Exercise 19 - Doubles
    - Refactor Unit Tests to use doubles

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -14,9 +14,9 @@ describe DockingStation do
 
   describe '#release_bike' do
     it "releases a bike" do
-      bike = Bike.new
-      subject.dock(bike)
-      expect(subject.release_bike).to eq bike
+      subject.dock double(:bike)
+      bike = subject.release_bike
+      expect(bike).to eq bike
     end
 
     it "raises error when no bikes available" do
@@ -24,15 +24,14 @@ describe DockingStation do
     end
 
     it "'releases' a working bike" do
-      bike = Bike.new
-      subject.dock(bike)
-      expect(subject.release_bike.working?).to eq true
+      subject.dock double(:bike)
+      bike = subject.release_bike
+      expect(bike.working?).to eq true
     end
 
     it "does not 'releases' a broken bike" do
-      bike = Bike.new
-      bike.report_broken
-      subject.dock(bike)
+      bike = subject.dock double(:bike)
+      bike.report_broken 
       expect { subject.release_bike }.to raise_error "No working bikes available"
     end
 
@@ -40,24 +39,23 @@ describe DockingStation do
 
   describe '#dock' do
     it "Raises an error if at capacity" do
-      DockingStation::DEFAULT_CAPACITY.times { subject.dock(Bike.new) }
-      expect { subject.dock(Bike.new) }.to raise_error 'No space available'
+      DockingStation::DEFAULT_CAPACITY.times { subject.dock double(:bike) }
+      expect { subject.dock double(:bike) }.to raise_error 'No space available'
     end
 
     it "docks something" do
-      bike = Bike.new
-      expect(subject.dock(bike)).to eq [bike]
+      bike = subject.dock double(:bike)
+      expect(bike).to eq [bike]
     end
 
     it "returns docked bikes" do
-      bike = Bike.new
-      subject.dock(bike)
+      bike = subject.dock double(:bike)
       expect(subject.bikes).to eq [bike]
     end
   end
 
   it "returns the attribute of the docked bike" do
-    bike = Bike.new
+    bike = subject.dock double(:bike)
     expect(subject.bike_available?(bike)).to eq bike
   end
 
